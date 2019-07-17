1b7cf39573a3ece75e17044fbb81a446a9e732f0
Simon Tooke <stooke@redhat.com>
2019-07-17 12:23:09 -0400

    Update CoffUtilMain.java

--- src/main/java/com/redhat/coffutil/CoffUtilMain.java
+++ src/main/java/com/redhat/coffutil/CoffUtilMain.java
@@ -11,14 +11,7 @@ public class CoffUtilMain {
     }
 
     public void run (String[] args) {
-        String fn = "c:/tmp/graal-8/openjdk-8u-212_01-src/build/windows-x86_64-normal-server-fastdebug/jdk/objs/javac_objs/main.obj";
-        fn = "c:/tmp/graal-8/prebuilt_jdk8_vanilla/bin/clhsdb.exe";
-        fn = "c:/tmp/vsjunk/ConsoleApplication1/ConsoleApplication1/x64/Debug/ConsoleApplication1.obj";
-        fn = "c:/tmp/vsjunk/ConsoleApplication1/x64/Debug/ConsoleApplication1.exe";
-        //fn = "C:\\tmp\\graal-8\\tmpfilesFromHello_after\\hello.obj";
-        fn = "c:/tmp/graal-8/hellotest/newdebugformat/hello.obj";
-        fn = args.length == 0 ?  fn  : args[0];
-
+        String fn = args.length == 0 ?  "test.obj"  : args[0];
         PECoffObjectFile cf = readCoff(fn);
     }
 
