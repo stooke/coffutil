ad862aa90b26d136537bd4b2eb4b64b1466ecc21
Simon Tooke <stooke@redhat.com>
2020-02-12 09:49:07 -0500

    some code for mach-o

--- src/main/java/com/redhat/coffutil/macho/fat/MachOFatBinary.java
+++ src/main/java/com/redhat/coffutil/macho/fat/MachOFatBinary.java
@@ -3,10 +3,98 @@ package com.redhat.coffutil.macho.fat;
 import com.redhat.coffutil.ExeFile;
 
 import java.io.PrintStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class MachOFatBinary implements ExeFile {
+
+    static final int FAT_MAGIC = 0xCAFEBABE; /* must be read in big-endian */
+
+    private ArrayList<FatArch> headers;
+
     @Override
     public void dump(PrintStream out) {
-        /* TODO */
+        out.format("Mach-O fat binary:\n");
+        for (FatArch arch : headers) {
+            out.format("  %s\n", arch);
+        }
+    }
+
+    ByteBuffer get(int i) {
+        FatArch arch = headers.get(i);
+        return null; /* TODO */
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MachOFatBinary(n=%d)", headers.size());
+    }
+
+    public ArrayList<FatArch> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(ArrayList<FatArch> headers) {
+        this.headers = headers;
+    }
+
+    static class FatArch {
+        private int cpuType;
+        private int cpuSubtype;
+        private long offset;
+        private long size;
+        private long align;
+
+        FatArch(int cputype, int cpuSubtype, long offset, long size, long align) {
+            this.cpuType = cputype;
+            this.cpuSubtype = cpuSubtype;
+            this.offset = offset;
+            this.size = size;
+            this.align = align;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("FatArch(%d.%d 0x%x 0x%x %d)", cpuType, cpuSubtype, offset, size, align);
+        }
+        public int getCpuType() {
+            return cpuType;
+        }
+
+        public void setCpuType(int cpuType) {
+            this.cpuType = cpuType;
+        }
+
+        public int getCpuSubtype() {
+            return cpuSubtype;
+        }
+
+        public void setCpuSubtype(int cpuSubtype) {
+            this.cpuSubtype = cpuSubtype;
+        }
+
+        public long getOffset() {
+            return offset;
+        }
+
+        public void setOffset(long offset) {
+            this.offset = offset;
+        }
+
+        public long getSize() {
+            return size;
+        }
+
+        public void setSize(long size) {
+            this.size = size;
+        }
+
+        public long getAlign() {
+            return align;
+        }
+
+        public void setAlign(long align) {
+            this.align = align;
+        }
     }
 }
--- src/main/java/com/redhat/coffutil/macho/fat/MachOFatBinaryBuilder.java
+++ src/main/java/com/redhat/coffutil/macho/fat/MachOFatBinaryBuilder.java
@@ -2,13 +2,39 @@ package com.redhat.coffutil.macho.fat;
 
 import com.redhat.coffutil.ExeFile;
 import com.redhat.coffutil.ExeFileBuilder;
+import com.redhat.coffutil.pecoff.Util;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
 
 public class MachOFatBinaryBuilder implements ExeFileBuilder {
     @Override
     public ExeFile build(File file) throws IOException {
+        ByteBuffer in = Util.readFile(file);
+        in.order(ByteOrder.BIG_ENDIAN);
+        int magic = in.getInt();
+        assert magic == MachOFatBinary.FAT_MAGIC;
+        int numArch = in.getInt();
+        MachOFatBinary fatfile = new MachOFatBinary();
+        fatfile.setHeaders(new ArrayList<>(numArch));
+        for (int i=0; i<numArch; i++) {
+            int cputype = in.getInt();
+            int cpuSubtype = in.getInt();
+            long offset = in.getInt();
+            long size = in.get();
+            long align = in.get();
+            MachOFatBinary.FatArch arch = new MachOFatBinary.FatArch(cputype, cpuSubtype, offset, size, align);
+            fatfile.getHeaders().add(arch);
+        }
         return null;
+    }
+
+    static boolean isAFatFile(File file, ByteBuffer in) {
+        in.order(ByteOrder.BIG_ENDIAN);
+        int magic = in.getInt(0);
+        return magic == MachOFatBinary.FAT_MAGIC;
     }
 }
